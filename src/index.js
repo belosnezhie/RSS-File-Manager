eb1ebf1dc43f5e3f224533d4c4c4131e2f857ead
Maria Treier <mariatraier@gmail.com>
2025-05-04 23:14:17 +0200

    feat: add .exit

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import { handleInputError } from './utils/handleInputError.js';
 import { printDirectory } from './utils/printDirectory.js';
 
 const main = async () => {
-
   const args = process.argv;
   const index = args.findIndex((i) => i.includes('--username'));
-  if (index === -1) {
-    throw new Error('Please, provide username')
-  }
-  const name =  args[index].split('=')[1];
+
+  const name = index === -1 ? 'Anonymous' : args[index].split('=')[1];
 
   console.log(`Welcome to the File Manager, ${name}!`);
   printDirectory();
 
   process.stdin.on('data', async (inputStdin) => {
     const input = inputStdin.toString().trim().split(' ');
+    if (input[0] === '.exit') {
+      handleExit(name);
+    }
     const controller = commandsMap.get(input[0]);
     const args = input.splice(1, input.length);
 
@@ -28,11 +28,15 @@ const main = async () => {
   });
 
   process.on('SIGINT', () => {
-    console.log(`\nThank you for using File Manager, ${name}, goodbye!`);
-
-    process.exit(0);
+    handleExit(name);
   });
 }
 
+
+const handleExit = (username) => {
+  console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
+
+  process.exit(0);
+}
 
 await main();
